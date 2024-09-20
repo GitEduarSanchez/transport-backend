0d47fceafc59af337dba19ba9265724110bf6b07
Eduar Sanchez <leosanchez_19@hotmail.com>
2024-09-20 17:11:48 -0500

    add controller Conductor

--- Poliedro.Billing.Api/Controllers/v1/Conductor/ConductorController.cs
+++ Poliedro.Billing.Api/Controllers/v1/Conductor/ConductorController.cs
@@ -2,14 +2,14 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Poliedro.Billing.Application.Common.Exeptions;
-using Poliedro.Billing.Application.Server.Commands.CreateServerCommand;
+using Poliedro.Billing.Application.Conductor.Commands.CreateServerCommand;
 
 namespace Poliedro.Billing.Api.Controllers.v1.Server
 {
     [Route("api/[controller]")]
     [ApiController]
     [TypeFilter(typeof(ExceptionManager))]
-    public class ServerController(IMediator mediator) : ControllerBase
+    public class ConductorController(IMediator mediator) : ControllerBase
     {
         [HttpGet("{id}")]
         public string Get(int id)
@@ -20,14 +20,14 @@ namespace Poliedro.Billing.Api.Controllers.v1.Server
 
         [HttpPost]
                 
-        public async Task<IActionResult> Create([FromBody] CreateServerCommand command)
+        public async Task<ActionResult<bool>> Create([FromBody] CreateConductorCommand command)
         {
             await mediator.Send(command);
             return CreatedAtAction(null, null);
         }
        
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] CreateServerCommand command)
+        public void Put(int id, [FromBody] CreateConductorCommand command)
         {
         }
 
---
+++ Poliedro.Billing.Infraestructure.Persistence.Mysql/Conductor/Adapter/ConductorRepository.cs
@@ -0,0 +1,14 @@
+﻿using Poliedro.Billing.Domain.Conductor.Entities;
+using Poliedro.Billing.Domain.Conductor.Ports;
+using Poliedro.Billing.Infraestructure.Persistence.Mysql.Context;
+
+namespace Poliedro.Billing.Infraestructure.Persistence.Mysql.Conductor.Adapter;
+
+public class ConductorRepository(DataBaseContext context) : IConductorRepository
+{
+    public async Task<bool> SaveAsync(ConductorEntity conductor)
+    {
+        await context.Conductor.AddAsync(conductor);
+        return  await context.SaveChangesAsync() > 0;
+    }
+}
