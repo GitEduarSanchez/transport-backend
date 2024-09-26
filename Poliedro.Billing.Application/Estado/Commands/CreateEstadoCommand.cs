215c92ec6918be3266709c90d1e10fae9a7b77bd
Diego <alejandrodiegoqc@gmail.com>
2024-09-25 19:01:13 -0500

    add controller estado

--- Poliedro.Billing.Application/Estado/Commands/CreateEstadoCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace Poliedro.Billing.Application.Conductor.Commands.CreateServerCommand;
-
-public record CreateEstadoCommand(string Descripcion) : IRequest<bool>;
--- Poliedro.Billing.Domain/Estado/Entities/Ports/IEstadoRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Poliedro.Billing.Domain.Conductor.Entities;
-
-namespace Poliedro.Billing.Domain.Conductor.Ports;
-
-public interface IEstadoRepository
-{
-    Task<bool> SaveAsync(EstadoEntity estado);
-}
