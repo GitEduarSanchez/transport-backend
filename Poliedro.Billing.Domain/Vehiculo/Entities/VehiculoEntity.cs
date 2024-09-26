256ea64baf37e342af197c753a666897ca774fdc
jeanka1 <jeanka7032@gmail.com>
2024-09-25 19:45:09 -0500

    add controller vehiculo

---
+++ Poliedro.Billing.Domain/Vehiculo/Entities/VehiculoEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Poliedro.Billing.Domain.Conductor.Entities;
+
+public class VehiculoEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ Poliedro.Billing.Infraestructure.Persistence.Mysql/Vehiculo/Adapter/VehiculoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Poliedro.Billing.Domain.Conductor.Entities;
+using Poliedro.Billing.Domain.Conductor.Ports;
+using Poliedro.Billing.Infraestructure.Persistence.Mysql.Context;
+
+namespace Poliedro.Billing.Infraestructure.Persistence.Mysql.Conductor.Adapter;
+
+public class VehiculoRepository(DataBaseContext context) : IVehiculoRepository
+{
+    public async Task<bool> SaveAsync(VehiculoEntity Vehiculo)
+    {
+        await context.Conductor.AddAsync(Vehiculo);
+        return  await context.SaveChangesAsync() > 0;
+    }
+}
