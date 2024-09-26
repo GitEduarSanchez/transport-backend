492cdff8e7cecd2f22354c059df93065cef74e59
Gerson Benitez <gersonbenirp27@gmail.com>
2024-09-25 19:00:31 -0500

    add  ConceptoController

---
+++ Poliedro.Billing.Application/Concepto/Commands/CreateConceptoCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Poliedro.Billing.Application.Conductor.Commands.CreateServerCommand;
+
+public record CreateConceptoCommand(int IdConcepto,string Descripcion) : IRequest<bool>;
---
+++ Poliedro.Billing.Domain/Concepto/Entities/ConceptoEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Poliedro.Billing.Domain.Conductor.Entities;
+
+public class ConceptoEntity
+{
+    [Key]
+    public int IdConcepto { get; set; }
+    public string Descripcion { get; set; }
+}
