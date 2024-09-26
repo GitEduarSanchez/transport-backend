4fb248317b82573f441ae127162a396fe297e340
yeny noriega <yenynoriega106@gmail.com>
2024-09-25 21:12:46 -0500

    ConductorConfiguration

--- Poliedro.Billing.Infraestructure.Persistence.Mysql/Trailer/Adapter/TrailerRepository.cs
+++ Poliedro.Billing.Infraestructure.Persistence.Mysql/Trailer/Adapter/TrailerRepository.cs
@@ -2,13 +2,13 @@
 using Poliedro.Billing.Domain.Conductor.Ports;
 using Poliedro.Billing.Infraestructure.Persistence.Mysql.Context;
 
-namespace Poliedro.Billing.Infraestructure.Persistence.Mysql.Conductor.Adapter;
+namespace Poliedro.Billing.Infraestructure.Persistence.Mysql.Trailer.Adapter;
 
-public class ConductorRepository(DataBaseContext context) : IConductorRepository
+public class trailerRepository(DataBaseContext context) : ITrailerRepository
 {
-    public async Task<bool> SaveAsync(ConductorEntity conductor)
+    public async Task<bool> SaveAsync(TrailerEntity trailer)
     {
-        await context.Conductor.AddAsync(conductor);
+        await context.Conductor.AddAsync(Trailer);
         return  await context.SaveChangesAsync() > 0;
     }
 }
