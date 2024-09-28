a644958be7ac0baeaccfb2e4c8966365d9e69196
AndreyArevalo <andreyarevalo0202@gmail.com>
2024-09-27 20:59:22 -0500

    add Handle Descargue

---
+++ Poliedro.Billing.Application/Descargue/Handle/DescargueHandle.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Poliedro.Billing.Application.Conductor.Commands.CreateServerCommand;
+using Poliedro.Billing.Domain.Conductor.Entities;
+using Poliedro.Billing.Domain.Conductor.Ports;
+
+namespace Poliedro.Billing.Application.Descargue.Handle;
+
+public class DescargueHandle(IDescargueRepository _descargueRepository) : IRequestHandler<CreateDescargueCommand, bool>
+{
+    public async Task<bool> Handle(CreateDescargueCommand request, CancellationToken cancellationToken)
+    {
+        DescargueEntity descargue = new() { Name = request.Name };
+        return await _DescargueRepository.SaveAsync(descargue);
+    }
+}
