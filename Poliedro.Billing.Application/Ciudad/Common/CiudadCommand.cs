ba6d66a6ebacb2ac9474feef9383edd25306e31a
maikel <maikelvanegasholos3@gmail.com>
2024-09-27 20:23:03 -0500

    add controller Ciudad

---
+++ Poliedro.Billing.Application/Ciudad/Common/CiudadCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Poliedro.Billing.Application.ciudad.Commands.CreateServerCommand;
+
+public record CreateciudadCommand(int Idciudad, string Descripcion, int Iddepartamento) : IRequest<bool>;
---
+++ Poliedro.Billing.Infraestructure.Persistence.Mysql/EntityFramework/EntityConfigurations/CiuadadConfiguration copy.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Poliedro.Billing.Domain.Conductor.Entities;
+
+namespace Poliedro.Billing.Infraestructure.Persistence.Mysql.EntityFramework.EntityConfigurations;
+
+public class ConductorConfiguration
+{
+    public ConductorConfiguration(EntityTypeBuilder<ConductorEntity> builder)
+    {
+        builder.ToTable("conductor");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasColumnName("idconductor");
+        builder.Property(x => x.Name).HasColumnName("nombre");
+    }
+}
