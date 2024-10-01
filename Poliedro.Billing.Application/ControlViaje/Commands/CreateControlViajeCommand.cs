0a74b6a117622ec5ff502a78c17e9d5c87dd72bd
brayan <bcdelgadoc@ufp`so.edu.co>
2024-10-01 09:45:07 -0500

    add ControlViajeHandle

--- Poliedro.Billing.Application/ControlViaje/Commands/CreateControlViajeCommand.cs
+++ Poliedro.Billing.Application/ControlViaje/Commands/CreateControlViajeCommand.cs
@@ -2,4 +2,4 @@
 
 namespace Poliedro.Billing.Application.ControlViaje.Commands.CreateServerCommand;
 
-public record CreateControlViajeCommand(int idControlViaje, DateTime fecha, string guia, int idVehiculoTrailer) : IRequest<bool>;
+public record CreateControlViajeCommand(DateTime fecha, string guia, int idVehiculoTrailer) : IRequest<bool>;
--- Poliedro.Billing.Application/ControlViaje/Handle/ControlViajeHandle.cs
+++ Poliedro.Billing.Application/ControlViaje/Handle/ControlViajeHandle.cs
@@ -3,13 +3,13 @@ using Poliedro.Billing.Application.ControlViaje.Commands.CreateServerCommand;
 using Poliedro.Billing.Domain.ControlViaje.Entities;
 using Poliedro.Billing.Domain.ControlViaje.Ports;
 
-namespace Poliedro.Billing.Application.Conductor.Handle;
+namespace Poliedro.Billing.Application.ControlViaje.Handle;
 
 public class ControlViajeHandle(IControlViajeRepository _controlviajeRepository) : IRequestHandler<CreateControlViajeCommand, bool>
 {
     public async Task<bool> Handle(CreateControlViajeCommand request, CancellationToken cancellationToken)
     {
-        ControlViajeEntity controlviaje = new() { idControlViaje = request.idControlViaje, fecha = request.fecha, guia = request.guia, idVehiculoTrailer = request.idVehiculoTrailer};
+        ControlViajeEntity controlviaje = new() {fecha = request.fecha, guia = request.guia, idVehiculoTrailer = request.idVehiculoTrailer};
         return await _controlviajeRepository.SaveAsync(controlviaje);
     }
 }
