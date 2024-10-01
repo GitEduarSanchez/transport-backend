abae6183d1edb50ab40ec53bf0028a793d411a7d
greidin villamizar <greidinvp@gmail.com>
2024-09-30 21:31:43 -0500

    add greidin

--- Poliedro.Billing.Application/Producto/commands/CreateProductoCommand.cs
+++ Poliedro.Billing.Application/Producto/commands/CreateProductoCommand.cs
@@ -2,4 +2,4 @@
 
 namespace Poliedro.Billing.Application.Producto.Commands.CreateServerCommand;
 
-public record CreateProductoCommand(int idProducto, String descripcion, int UnidadMedida) : IRequest<bool>;
+public record CreateProductoCommand(string descripcion, int UnidadMedida) : IRequest<bool>;
