e47ed91a7b19b096f5eed6cc6c7b719e88835a59
brayan <bcdelgadoc@ufp`so.edu.co>
2024-10-01 10:06:36 -0500

    add ControlViajeDto

---
+++ Poliedro.Billing.Application/ControlViaje/Dto/ControlViajeDto.cs
@@ -0,0 +1,6 @@
+﻿using System.Data.Common;
+
+namespace Poliedro.Billing.Application.ControlViaje.Dto;
+
+public record ControlViajeDto(DateTime fecha, string guia, int idVehiculoTrailer);
+
---
+++ Poliedro.Billing.Application/ControlViaje/Query/GetAllActuatorHandle.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Poliedro.Billing.Application.ControlViaje.Dto;
+using Poliedro.Billing.Domain.ControlViaje.Ports;
+
+namespace Poliedro.Billing.Application.ControlViaje.Query;
+
+public class GetAllActuatorHandle(IControlViajeRepository _controlviajeRepository) : IRequestHandler<GetAllActuatorsQuery, IEnumerable<ControlViajeDto>>
+{
+    public async Task<IEnumerable<ControlViajeDto>> Handle(GetAllActuatorsQuery request, CancellationToken cancellationToken)
+    {
+        var entities = await _controlviajeRepository.GetAllAsync();
+        return entities.Select(controlviaje => new ControlViajeDto
+        (
+            fecha: controlviaje.fecha, 
+            guia: controlviaje.guia,
+            idVehiculoTrailer: controlviaje.idVehiculoTrailer
+        ));
+    }
+}
