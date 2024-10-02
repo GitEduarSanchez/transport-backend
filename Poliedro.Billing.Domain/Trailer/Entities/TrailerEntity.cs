5fbe0ebf089171f4b542dcb1e962d7b4fe04d649
yeny noriega <yenynoriega106@gmail.com>
2024-10-01 19:56:20 -0500

    add

--- Poliedro.Billing.Domain/Trailer/Entities/TrailerEntity.cs
+++ Poliedro.Billing.Domain/Trailer/Entities/TrailerEntity.cs
@@ -5,7 +5,6 @@ namespace Poliedro.Billing.Domain.Trailer.Entities;
 public class TrailerEntity
 {
     [Key]
-    public int idTrailer { get; set; }
     public string Descripcion { get; set; }
     public string serial { get; set; }
 }
