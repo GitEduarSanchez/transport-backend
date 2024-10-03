27d2a9b1f0d517a0be24769633d91a6743ed0d1b
AndreyArevalo <andreyarevalo0202@gmail.com>
2024-10-02 19:20:19 -0500

    add controlles descargue

--- Poliedro.Billing.Api/Controllers/Descargue/DescargueController.cs
+++ Poliedro.Billing.Api/Controllers/Descargue/DescargueController.cs
@@ -3,6 +3,8 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Poliedro.Billing.Application.Common.Exeptions;
 using Poliedro.Billing.Application.Descargue.Commands.CreateServerCommand;
+using Poliedro.Billing.Application.Descargue.Dto;
+using Poliedro.Billing.Application.Descargue.Query;
 
 namespace Poliedro.Billing.Api.Controllers.v1.Server
 {
@@ -11,6 +13,12 @@ namespace Poliedro.Billing.Api.Controllers.v1.Server
     [TypeFilter(typeof(ExceptionManager))]
     public class DescargueController(IMediator mediator) : ControllerBase
     {
+        [HttpGet]
+        public async Task<IEnumerable<DescargueDto>> GetAll()
+        {
+            return await mediator.Send(new GetAllActuatorsQuery());
+        }
+
         [HttpGet("{id}")]
         public string Get(int id)
         {
