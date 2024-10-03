0d2a8eecd08aca5abec8a4a6ac4a2ba425183d19
Gustavo duran <gadavo202@gmail.com>
2024-10-03 18:30:43 -0500

    Add OrigenController

---
+++ Poliedro.Billing.Api/Origen/Controllers/OrigenController.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Poliedro.Billing.Application.Common.Exeptions;
+using Poliedro.Billing.Application.Origen.Commands.CreateServerCommand;
+
+namespace Poliedro.Billing.Api.Controllers.v1.Server
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [TypeFilter(typeof(ExceptionManager))]
+    public class OrigenController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        [HttpPost]
+                
+        public async Task<ActionResult<bool>> Create([FromBody] CreateOrigenCommand command)
+        {
+            await mediator.Send(command);
+            return CreatedAtAction(null, null);
+        }
+       
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] CreateOrigenCommand command)
+        {
+        }
+
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        private IActionResult HandleValidationErrors(List<ValidationFailure> errors)
+        {
+            GetErrorValidator(errors);
+            return BadRequest(errors);
+        }
+
+        private static void GetErrorValidator(List<ValidationFailure> failures)
+        {
+            foreach (var failure in failures)
+            {
+                Console.WriteLine($"log: {failure.ErrorMessage}");
+            }
+        }
+    }
+
+    public class CreateOrigenCommand
+    {
+    }
+}
---
+++ Poliedro.Billing.Application/Origen/Commands/CreateOrigenCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Poliedro.Billing.Application.Origen.Commands.CreateServerCommand;
+
+public record CreateOrigenCommand(string descripcion) : IRequest<bool>;
