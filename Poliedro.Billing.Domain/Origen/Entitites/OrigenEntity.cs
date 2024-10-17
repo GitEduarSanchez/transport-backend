4303d0a707a01645498a5ac8e5798bf1564296ce
Gustavo duran <gadavo202@gmail.com>
2024-10-17 15:31:56 -0500

    NUE

---
+++ Poliedro.Billing.Domain/Origen/Entitites/OrigenEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Poliedro.Billing.Domain.Origen.Entities;
+
+public class OrigenEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public string descripcion { get; set; }
+}
